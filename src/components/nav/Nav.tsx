707e5a2226a8879db7db7a63a8bc757bd5ee0b8e
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-05 00:04:24 -0300

    feat: amnimaciones

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,20 +1,48 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { NAV_TRANSITION } from "@/helpers/transitions";
 
 interface Props {}
+
+const NAV_JOB_TRANSITION = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    duration: 10,
+    erase: [1.93, 0, 0.17, 1],
+  },
+  exit: { opacity: 0 },
+};
+
+const NAV_NAME_TRANSITION = {
+  initial: { opacity: 0, y: 100 },
+  animate: { opacity: 1, y: 0 },
+  transition: { duration: 1, ease: "easeOut", delay: 0.2 },
+  exit: { opacity: 0 },
+};
+
+const TECNOLOGIES_TRANSITION = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    dealy: 10,
+  },
+  transition: { duration: 1, ease: "easeIn", delay: 0.2 },
+  exit: { opacity: 0 },
+};
 
 export const Nav = (props: Props) => {
   return (
     <div className="flex flex-col items-center max-w-[36rem]">
-      <motion.h1 {...NAV_TRANSITION} className="text-5xl mb-4">
+      <motion.h1 {...NAV_NAME_TRANSITION} className="text-5xl mb-4">
         Roxana Narela Camara
       </motion.h1>
-      <motion.h1 {...NAV_TRANSITION} className="text-3xl mb-4">
+      <motion.h1 {...NAV_JOB_TRANSITION} className="text-3xl mb-4">
         Semi Senior Frontend developer
       </motion.h1>
-      <motion.h1 {...NAV_TRANSITION} className="text-xl mb-4">
+      <motion.h1 {...TECNOLOGIES_TRANSITION} className="text-xl mb-4">
         Next | Node | React Native
       </motion.h1>
     </div>
