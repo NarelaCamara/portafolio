8afde786c52a7d03160104f4cf6c210f6ed84f12
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-05 02:34:20 -0300

    feat: circle

--- src/components/circle/Circle.tsx
+++ src/components/circle/Circle.tsx
@@ -7,11 +7,18 @@ interface Props {}
 export const Circle = (props: Props) => {
   return (
     <motion.div
-      className="bg-white border border-cyan-300 p-1"
+      drag
+      dragConstraints={{
+        top: -30,
+        left: -30,
+        right: 30,
+        bottom: 30,
+      }}
+      className=" border border-cyan-300 p-20 rounded-full m-8"
       animate={{
         scale: [1, 2, 2, 1, 1],
         rotate: [0, 0, 270, 270, 0],
-        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        borderRadius: ["0%", "25%", "50%", "75%", "100%"],
       }}
     />
   );
