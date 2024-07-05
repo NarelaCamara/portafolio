8c46cd0ad2cf7b5540de24e6dfcd054c208e9285
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-04 23:25:18 -0300

    feat: animations

--- src/components/circle/Circle.tsx
+++ src/components/circle/Circle.tsx
@@ -6,15 +6,13 @@ interface Props {}
 
 export const Circle = (props: Props) => {
   return (
-    <div>
-      <motion.div
-        className="bg-white border border-cyan-300 p-12"
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-        }}
-      />
-    </div>
+    <motion.div
+      className="bg-white border border-cyan-300 p-1"
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+    />
   );
 };
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,13 +1,22 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import { NAV_TRANSITION } from "@/helpers/transitions";
 
 interface Props {}
 
 export const Nav = (props: Props) => {
   return (
     <div className="flex flex-col items-center max-w-[36rem]">
-      <span className="text-5xl mb-4">Roxana Narela Camara</span>
-      <span className="text-3xl mb-4">Semi Senior Frontend developer</span>
-      <span className="text-xl mb-4"> Next | Node | React Native</span>
+      <motion.h1 {...NAV_TRANSITION} className="text-5xl mb-4">
+        Roxana Narela Camara
+      </motion.h1>
+      <motion.h1 {...NAV_TRANSITION} className="text-3xl mb-4">
+        Semi Senior Frontend developer
+      </motion.h1>
+      <motion.h1 {...NAV_TRANSITION} className="text-xl mb-4">
+        Next | Node | React Native
+      </motion.h1>
     </div>
   );
 };
