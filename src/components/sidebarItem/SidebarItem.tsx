2d1dc4d66dd7bc071afe33c3894375b321ec7d07
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-05 00:24:12 -0300

    feat: sidebar

--- src/components/sidebarItem/SidebarItem.tsx
+++ src/components/sidebarItem/SidebarItem.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 interface Props {
   text: string;
   icon: any;
@@ -6,12 +8,18 @@ interface Props {
 
 export const SidebarItem = ({ text, icon }: Props) => {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0.6 }}
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.2 },
+      }}
+      whileInView={{ opacity: 1 }}
       role="button"
       tabIndex={0}
       className="flex items-stretch w-full p-3 rounded-lg text-start text-lg leading-tight transition-all hover:-translate-y-2 hover:text-blue-400 "
     >
       {text}
-    </div>
+    </motion.div>
   );
 };
