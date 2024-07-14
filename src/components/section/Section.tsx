d5b5e153f2ca4641290c86ad1b2c06114ff86c58
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-14 16:12:54 -0300

    feat:

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,15 +1,23 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 interface Props {
   children: any;
   title: string;
 }
 
+const SECTION_TRANSITION = {};
+
 export const Section = ({ title, children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col flex-nowrap justify-center items-start w-4/5 pb-16">
-      <span className="text-3xl font-bold py-8">{title}</span>
-      <div className="my-4"> {children}</div>
-    </div>
+    <motion.section
+      id={title}
+      className="min-h-screen flex flex-col flex-nowrap justify-center items-start  pb-16"
+    >
+      <span className="text-5xl py-8 m-4">{title}</span>
+
+      <motion.div className="m-4 text-clip ">{children}</motion.div>
+    </motion.section>
   );
 };
