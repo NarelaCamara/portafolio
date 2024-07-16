dca4c47eabc49a86bc8fdc33ac15235630be30f9
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-15 21:51:12 -0300

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,14 +44,14 @@ export default function Home() {
         ></script>
       </head>
       <body>
-        <main className="min-h-screen flex flex-row relative">
+        <main className="min-h-screen flex flex-row relative m-24">
           <div className="fixed w-screen p-0 m-0 top-0">
             <motion.div
               style={{ scaleX: scrollYProgress }}
               className="p-2 bg-gray-200 w-full opacity-15"
             />
           </div>
-          <div className="flex flex-col top-60 items-center fixed z-10 py-10 p-6">
+          <div className="flex flex-col top-60 items-center fixed z-10 ">
             <Nav />
             <Sidebar />
           </div>
