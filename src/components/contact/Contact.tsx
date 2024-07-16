c8c157cceaf1ecad210526cafb57350fff9df2a5
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-15 23:42:28 -0300

    feat: comtact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -10,31 +10,31 @@ interface Props {}
 
 export const Contact = (props: Props) => {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col justify-start">
       <div className="relative">
-        <span className="flex flex-row justify-start  py-2  w-full p-3 rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
-          <FaLocationDot size="2.5rem" className="mx-1" />
+        <span className="flex flex-row justify-start  py-2  w-full  rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
+          <FaLocationDot size="2.5rem" className="mx-4" />
           <span className="text-xl">Argentina, Buenos Aires</span>
         </span>
       </div>
 
       <div className="relative">
-        <span className="flex flex-row justify-start  py-2  w-full p-3 rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
-          <FaLinkedin size="2.5rem" className="mx-1" />
+        <span className="flex flex-row justify-start  py-2  w-full  rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
+          <FaLinkedin size="2.5rem" className="mx-4" />
           <span className="text-xl">narelacamara</span>
         </span>
       </div>
 
       <div className="relative">
-        <span className="flex flex-row justify-start  py-2  w-full p-3 rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
-          <FaEnvelope size="2.5rem" className="mx-1" />
+        <span className="flex flex-row justify-start  py-2  w-full  rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
+          <FaEnvelope size="2.5rem" className="mx-4" />
           <span className="text-xl">Narela.camara@gmail.com</span>
         </span>
       </div>
 
       <div className="relative">
-        <span className="flex flex-row justify-start  py-2  w-full p-3 rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
-          <FaGithub size="2.5rem" className="mx-1" />
+        <span className="flex flex-row justify-start  py-2  w-full  rounded-lg text-start leading-tight   font-normal text-base transition-all duration-[0.5s] transform-gpu hover:font-bold hover:scale-110 hover:translate-x-[-10px]">
+          <FaGithub size="2.5rem" className="mx-4" />
           <span className="text-xl">NarelaCamara</span>
         </span>
       </div>
