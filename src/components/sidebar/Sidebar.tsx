61259995a3c9a63c5ef3240ef9f3e36a9b12e811
Narela Camara <50678398+NarelaCamara@users.noreply.github.com>
2024-07-20 05:46:13 +0000

    feat: responsive

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ interface Props {}
 export const Sidebar = (props: Props) => {
   return (
     <>
-      <div className="flex max-xl:flex-row xl:flex-col max-xl:bg-origin-content  max-xl:fixed text-white  w-full xl:max-w-[15rem] p-4 max-xl:justify-center">
+      <div className="flex max-xl:flex-row xl:flex-col max-xl:bg-origin-content  max-xl:fixed text-white  w-full xl:max-w-[15rem] p-4 max-xl:justify-center bg-black max-xl:z-20">
         <nav className="flex  max-xl:flex-row  xl:flex-col gap-1 min-w-[240px]  text-lg font-normal text-white">
           {ITEMS.map((e) => (
             <SidebarItem key={e.title} {...e} />
