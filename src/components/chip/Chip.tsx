98f700f8c2948b07f489c4458e4b16465b004dc1
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-17 10:52:26 -0300

    refactor: change chip style

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export const Chip = ({ text }: Props) => {
   return (
-    <p className="inline-flex items-center justify-center min-w-20 p-2 min-h-6 m-1 uppercase text-xs font-semibold  rounded-full translate-y-0.5">
+    <span className="m-1 bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">
       {text}
-    </p>
+    </span>
   );
 };
