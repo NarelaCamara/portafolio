f9ef050168827da95e588cf7241dac67cccca0f0
Narela Camara <50678398+NarelaCamara@users.noreply.github.com>
2025-04-02 19:39:06 +0000

    feat: descargar cv

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -29,8 +29,14 @@ export const About = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <FaDownload size="1.5rem" className="w-3.5 h-3.5 me-2" />
-            Descarga CV
+            <a
+              href="/cv-NarelaRCamara.pdf"
+              download="cv-NarelaCamara.pdf"
+              rel="noopener noreferrer"
+            >
+              <FaDownload size="1.5rem" className="w-3.5 h-3.5 me-2" />
+              Descarga CV
+            </a>
           </button>
           <button
             type="button"
