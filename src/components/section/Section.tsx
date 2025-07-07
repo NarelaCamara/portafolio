865c0275ed415d0a941302c0a0c8aa7da1729d06
RoxanaCamara <roxana.camara07@gmail.com>
2025-07-07 01:25:20 -0300

    feat: section

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -8,14 +8,13 @@ interface Props {
 
 export const Section = ({ title, children }: Props) => {
   return (
-    <section id={title} className={`lg:relative lg:h-screen mt-20`}>
+    <section id={title} className={`lg:h-screen pt-32`}>
       {
         <p className=" p-3 font-bold font-fraunces text-2xl lg:text-4xl text-center lg:text-start">
           {title}
         </p>
       }
-
-      <div className="p-2 ">{children}</div>
+      {children}
     </section>
   );
 };
