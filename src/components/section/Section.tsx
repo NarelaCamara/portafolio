2bc9e0a03651d12de4aabaa7f2779b0f63f50a84
RoxanaCamara <roxana.camara07@gmail.com>
2025-07-07 14:12:41 -0300

    feat: padding

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const Section = ({ title, children }: Props) => {
   return (
-    <section id={title} className={`lg:h-screen pt-32`}>
+    <section id={title} className={`lg:h-screen lg:pt-32 md:pt-10 sm:pt-12`}>
       {
         <p className=" p-3 font-bold font-fraunces text-2xl lg:text-4xl text-center lg:text-start">
           {title}
