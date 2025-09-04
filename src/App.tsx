a752e7ad6c9a7f6910ea2d869e9ac58831a52258
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-04 15:38:19 -0300

    feat: modificaciones

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,16 @@ function App() {
 
   return (
     <div className="bg-slate-900 flex flex-nowrap flex-col lg:flex-row items-center">
-      <div className="relative h-screen lg:h-fit lg:fixed top-[15vh] lg:left-[3vw]">
+      <div className="relative h-screen lg:h-fit lg:fixed top-[15vh] lg:left-[8vw]">
         <Nav />
       </div>
 
-      <div className="fixed bottom-0 md:bottom-auto md:top-0 lg:left-[10vw]  flex flex-row items-center lg:flex-col  lg:top-[62%]  justify-center py-2 bg-[#0f172b] z-30 lg:z-0">
+      <div className="fixed bottom-0 md:bottom-auto md:top-0 lg:left-[14vw]  flex flex-row items-center lg:flex-col  lg:top-[62%]  justify-center py-2 bg-[#0f172b] z-30 lg:z-0">
         {ITEMS.map((e) => (
           <SidebarItem key={e.title} title={e.title} />
         ))}
       </div>
-      <div className="lg:max-w-min lg:relative lg:left-[40vw]">
+      <div className="lg:min-w-[45vw] lg:max-w-[55vw] lg:relative lg:left-[30vw]">
         {ITEMS.map((e) => (
           <Section {...e} key={e.title} />
         ))}
