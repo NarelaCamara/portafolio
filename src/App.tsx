ab53f3ef9f9ed3f27ba976120a82719b884fc7ae
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-04 17:53:33 -0300

    feat: corroeccion

--- src/App.tsx
+++ src/App.tsx
@@ -32,23 +32,27 @@ function App() {
 
   return (
     <div className="bg-slate-900 w-full min-h-screen flex flex-col lg:flex-row items-center">
-      <div className="max-lg:block hidden relative h-screen lg:h-fit lg:fixed top-[15vh] lg:left-[8vw]">
+      {/* Vista mobile */}
+      <div className="max-lg:block hidden relative h-screen lg:h-fit">
         <Nav />
       </div>
 
-      <div className="max-lg:block hidden fixed bottom-0 md:bottom-auto md:top-0 lg:left-[20vw]  flex flex-row items-center lg:flex-col  lg:top-[62%]  justify-center py-2 bg-[#0f172b] z-30 lg:z-0">
+      <div className="max-lg:block hidden fixed bottom-0 md:bottom-auto md:top-0 flex flex-row items-center justify-center py-2 bg-[#0f172b] z-30">
         {ITEMS.map((e) => (
           <SidebarItem key={e.title} title={e.title} />
         ))}
       </div>
 
-      <div className=" w-full max-w-[1204px] mx-auto flex flex-col lg:flex-row relative">
-        <div className="hidden lg:block w-full lg:w-[20%] lg:fixed lg:top-[10%] lg:left-0 flex flex-col items-center">
-          {/* SIDEBAR */}
-          <div className=" p-4 bg-slate-900">
+      {/* Contenedor centrado con ancho máximo */}
+      <div className="w-full max-w-[1204px] mx-auto flex flex-col lg:flex-row relative">
+        {/* NAV + SIDEBAR en desktop */}
+        <div className="hidden lg:flex lg:fixed lg:inset-y-0 lg:left-0 lg:w-[20%] flex-col items-center justify-center space-y-6">
+          {/* NAV */}
+          <div className="p-4 bg-slate-900">
             <Nav />
           </div>
-          <div className="flex flex-row lg:flex-col items-center justify-center py-2 bg-[#0f172b] z-30">
+          {/* SIDEBAR */}
+          <div className="flex flex-col items-center justify-center py-2 bg-[#0f172b] z-30">
             {ITEMS.map((e) => (
               <SidebarItem key={e.title} title={e.title} />
             ))}
@@ -56,7 +60,7 @@ function App() {
         </div>
 
         {/* SECTIONS */}
-        <div className="w-full lg:w-[85%] lg:ml-[20%] px-4 md:px-8">
+        <div className="w-full lg:w-[80%] lg:ml-[20%] px-4 md:px-8">
           {ITEMS.map((e) => (
             <Section {...e} key={e.title} />
           ))}
