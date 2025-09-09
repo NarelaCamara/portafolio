97fe0c1930124383087a64a877609393d15f9061
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-09 12:47:45 -0300

    feat: type ReactElement

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { type ReactElement } from "react";
+
 interface Props {
-  children: any;
+  children: ReactElement | ReactElement[];
   title: string;
 }
 
