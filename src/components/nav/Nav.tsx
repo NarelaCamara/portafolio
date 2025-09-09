889556cc99c233b416827021cbc754f77fdc375b
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-09 12:20:19 -0300

    feat: desing nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,8 +1,40 @@
+import { useEffect, useRef } from "react";
 import nare from "../../assets/nare.jpeg";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 export const Nav = () => {
+  const checkList = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(SplitText);
+
+    gsap.set(checkList.current, { opacity: 1 });
+
+    if (checkList.current) {
+      gsap.registerPlugin(SplitText);
+
+      // Crea un selector scoped al contenedor
+      const q = gsap.utils.selector(checkList.current);
+
+      // Selecciona todos los elementos h1 dentro del contenedor
+      const paragraphs = q("h1");
+
+      const split = SplitText.create(paragraphs, { type: "words,chars" });
+      //now animate each character into place from 20px below, fading in:
+      gsap.from(split.chars, {
+        y: 20,
+        autoAlpha: 0,
+        stagger: 0.01,
+      });
+    }
+  }, []);
+
   return (
-    <div className={`flex flex-col items-center justify-center  `}>
+    <div
+      ref={checkList}
+      className={`flex flex-col items-center justify-center  `}
+    >
       <img
         alt="User_photo"
         className="rounded-full border-2  border-[#6C7289] h-[256px] w-[256px]"
