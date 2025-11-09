bd692b4290644f91b2812abfc3cb1124639fd2e0
RoxanaCamara <roxana.camara07@gmail.com>
2025-11-09 13:39:45 -0300

    feat: contacto

--- src/components/career_path/career_path.tsx
+++ src/components/career_path/career_path.tsx
@@ -15,12 +15,6 @@ export const CareerPath = () => {
   ];
   return (
     <div>
-      <p
-        className={`my-3 text-[#6C7289] text-sm text-center justify-start font-[Montserrat]`}
-      >
-        A continuación, se detallan mis experiencias laborales y formativas más
-        relevantes:
-      </p>
       {ITEMS.map((e) => (
         <Section {...e} key={e.title} />
       ))}
--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,3 +1,28 @@
 export const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center gap-16 m-24">
+      <input
+        placeholder="Asunto"
+        type="text"
+        name="asunto"
+        id="asunto"
+        className="border-white bg-gray-800 rounded-3xl  px-4 py-2  text-white"
+      />
+      <input
+        placeholder="Email"
+        type="email"
+        name=""
+        id="email"
+        className="border-white bg-gray-800  rounded-3xl  px-4 py-2  text-white"
+      />
+      <textarea
+        placeholder="descripción"
+        name="descripcion"
+        maxLength={1024}
+        id="descripcion"
+        className="border-white bg-gray-800  rounded-3xl  px-4 py-2  text-white custom-resize-handle custom-scrollbar"
+        rows={4}
+      />
+    </div>
+  );
 };
